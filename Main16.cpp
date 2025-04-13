2b958846048d9e2e84bda4b16e1f5f7de450c3e0
YURI.XMT <ahmadpoorjavad742653@gmail.com>
2025-04-13 02:13:43 -0700

    pic 16

---
+++ Main16.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#ifndef SQUARE_H
+#define SQUARE_H
+
+int GetSquareSides() {
+    return 4;
+}
+
+#endif
+
+#ifndef GEOMETRY_H
+#define GEOMETRY_H
+
+#include "Square.h"
+
+#endif
+
+int main() {
+    return 0;
+}

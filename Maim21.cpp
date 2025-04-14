39728ece596e46a01b8ba86ed662f5fc03c90b84
YURI.XMT <ahmadpoorjavad742653@gmail.com>
2025-04-14 12:01:10 -0700

    pic 21

---
+++ Maim21.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    if (x == 0) {
+        std::cout << "The value is zero\n";
+    }
+    else if (x > 0) {
+        std::cout << "The value is +\n";
+    }
+    else {
+        std::cout << "The value is -\n";
+    }
+
+    return 0;
+}
